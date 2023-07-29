b42c5c3ee149feb69e455606a8ef9ed6f9e251be
ramanaroyal970 <renatiramana007@gmail.com>
2023-07-29 13:56:01 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def data_render(request):
     d={'name':'RAMANA','age':23}
     return render(request,'data_render.html',context=d)
+def if_else(request):
+    d={'a':19,'b':35}
+    return render(request,'if_else.html',context=d)
+def if_elif(request):
+    d={'a':19,'b':35,'c':42}
+    return render(request,'if_elif.html',context=d)
+def nested_if(request):
+    d={'a':19,'b':35,'c':42}
+    return render(request,'nested_if.html',context=d)
